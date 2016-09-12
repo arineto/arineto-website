65b7ce38c5b2509ceacaada5b05f23b29e683d4b
Arimatea Neto <arineto30@gmail.com>
2016-09-11 23:00:15 -0300

    Adding post details page

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from apps.blog import views
 
 urlpatterns = [
+    url(r'^(?P<slug>[-\w]+)/$', views.PostDetailView.as_view(),
+        name='post_details'),
     url(r'^$', views.PostListView.as_view(), name='home'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import DetailView
 from django.views.generic import ListView
 from apps.blog.models import Post
 
@@ -5,9 +6,15 @@ from apps.blog.models import Post
 class PostListView(ListView):
 
     model = Post
-    template_name = 'blog/base.html'
+    template_name = 'blog/post_list.html'
     paginate_by = 5
 
     def get_queryset(self):
         queryset = super(PostListView, self).get_queryset()
         return queryset.released()
+
+
+class PostDetailView(DetailView):
+
+    model = Post
+    template_name = 'blog/post_details.html'
